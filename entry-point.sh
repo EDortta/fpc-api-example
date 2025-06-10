4333cd8e99b81e1a7d9d6e267b606c32151249c7
t01 devel mode <root@Interno.ag.sh>
2025-06-10 13:10:07 +0000

    Better entry-point.sh when running under Windows's docker version

--- entry-point.sh
+++ entry-point.sh
@@ -3,14 +3,23 @@ set -e
 
 cd "$(dirname "$0")"
 
+dos2unix -n .env /tmp/.env
+
 # Load .env file
-if [ -f .env ]; then
-  export $(grep -v '^#' .env | xargs)
+if [ -f /tmp/.env ]; then
+  export $(grep -v '^#' /tmp/.env | xargs)
 fi
 
-# Validate important paths
+tries=5
+until [ -d "$PROJECT_DIR" ] || [ $tries -eq 0 ]; do
+  echo "Waiting for PROJECT_DIR ($PROJECT_DIR) to be available..."
+  sleep 1
+  tries=$((tries - 1))
+done
+
 if [ ! -d "$PROJECT_DIR" ]; then
   echo "Error: PROJECT_DIR not found: $PROJECT_DIR"
+  ls -lahrt /home/developer/Projects
   exit 1
 fi
 
